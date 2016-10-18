70b1144375c7e89484b6b43d6b4ddbcdf13e0323
Tyrell Jackson <tyrell.jackson@cardinalhealth.com>
2016-10-18 10:13:12 -0400

    Update deployToMarathon.sh

--- deployToMarathon.sh
+++ deployToMarathon.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
-echo FOR COMMAND
 for machine in ${CONTAINER}
 do
 MARATHON_HOST="run.cahcommtech.net:80"
@@ -8,16 +7,14 @@ MACHINE_DESC="deploy.json"
 
 TIMEOUT=600
 
-echo CHANGING WORKSPACE
 cd ${WORKSPACE}/$machine
 
 if [ -f "${MACHINE_DESC}" ]; then
-  echo SED COMMAND
+  
   sed -i -e "s/latest/" ${MACHINE_DESC}  
-  echo CURL COMMAND
+  
   curl -X PUT -H "Content-Type: application/json" "http://${MARATHON_HOST}/v2/apps/${MACHINE}" -d@"${MACHINE_DESC}"
 else
-  echo EXIT COMMAND
   exit -1
 fi
 
