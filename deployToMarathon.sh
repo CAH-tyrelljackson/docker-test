558b6d4998207c310136f0288a59c1df1150146f
Tyrell Jackson <tyrell.jackson@cardinalhealth.com>
2016-10-18 10:10:32 -0400

    Update deployToMarathon.sh

--- deployToMarathon.sh
+++ deployToMarathon.sh
@@ -7,12 +7,13 @@ MACHINE_DESC="deploy.json"
 
 TIMEOUT=600
 
+echo CHANGING WORKSPACE
 cd ${WORKSPACE}/$machine
 
 if [ -f "${MACHINE_DESC}" ]; then
-  
+  echo SED COMMAND
   sed -i -e "s/latest/" ${MACHINE_DESC}  
-
+  echo CURL COMMAND
   curl -X PUT -H "Content-Type: application/json" "http://${MARATHON_HOST}/v2/apps/${MACHINE}" -d@"${MACHINE_DESC}"
 else
   exit -1
